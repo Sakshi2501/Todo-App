2ab9a2c995b00f68be4a7aae5d71171790bb45fd
Sakshi <sakshi.wtpqwppr@gmail.com>
2024-01-23 11:36:32 +0530

    fixed update issue

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 
-const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState("")
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault(); 
-
-        addTodo(value);
-
-        setValue("");
-    }
+    const handleSubmit = (e) => {
+      // prevent default action
+        e.preventDefault();
+        if (value) {
+          // add todo
+          addTodo(value);
+          // clear form after submission
+          setValue('');
+        }
+      };
   return (
-    <form className='TodoForm' onSubmit={handleSubmit} >
-        <input type="text" className="todo-input" value={value} placeholder='Add New Task' 
-        onChange={(e) => setValue(e.target.value)}/>
-        <button type='submit' className='todo-btn'>Add Task</button>
-    </form>
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
   )
 }
-
-export default TodoForm;
